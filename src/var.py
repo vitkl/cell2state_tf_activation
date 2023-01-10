ababc889eaee8545b5d5e826fa1a724b592ce2d1
kchl5 <k.ly@hotmail.ch>
2023-01-10 12:14:31 +0000

    created file to explore protospacer cell ranger data

---
+++ src/var.py
@@ -0,0 +1,19 @@
+import datetime
+import os
+
+###Define constants here###
+
+#Current date format is YYYYMMDD with automatically updated date
+today = datetime.datetime.now().strftime("%Y%m%d")
+
+#Define paths to data here
+sc_data_folder = '/nfs/team205/vk7/sanger_projects/cell2state_tf_activation/data/pilot_5_prime_dec_2022/'
+
+#Save results in the following folder (create if it doesn't exist)
+
+results_folder = '/lustre/scratch123/hgi/teams/parts/kl11/cell2state_tf_activation/results/'
+if not os.path.exists(results_folder):
+    os.makedirs(results_folder)
+
+
+
