464546ac78707fb7579830dc8afff9b0bdee4d02
kchl5 <k.ly@hotmail.ch>
2023-02-02 11:58:40 +0000

    corrected plot labelling for guide assignment script

--- src/var.py
+++ src/var.py
@@ -1,6 +1,7 @@
 import datetime
 import os
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 ###Define constants here###
 
@@ -9,6 +10,7 @@ today = datetime.datetime.now().strftime("%Y%m%d")
 
 #Define paths to data here
 sc_data_folder = '/nfs/team205/vk7/sanger_projects/cell2state_tf_activation/data/pilot_5_prime_dec_2022/'
+sc_data_w_cas9_seq_folder = '/lustre/scratch123/hgi/teams/parts/kl11/data/'
 
 #Save results in the following folder (create if it doesn't exist)
 
@@ -17,3 +19,13 @@ if not os.path.exists(results_folder):
     os.makedirs(results_folder)
 
 color=sns.color_palette("colorblind", 10)
+
+#create matplotlib default options for plots to be loaded into jupyter notebook
+
+plt.rcParams['figure.dpi'] = 100 # 200 e.g. is really fine, but slower
+plt.rcParams['savefig.dpi'] = 300 
+#remove upper axis and right axis from all plots
+plt.rcParams['axes.spines.top'] = False
+plt.rcParams['axes.spines.right'] = False
+#legend without frame
+plt.rcParams['legend.frameon'] = False
